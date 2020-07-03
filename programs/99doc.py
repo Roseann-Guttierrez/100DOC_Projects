04ddaf66f3c05ec78801d2b8a8e96a3de5b54ac0
RoseG <60761075+Roseann-Guttierrez@users.noreply.github.com>
2020-07-02 23:05:48 -0500

    Create 99doc.py
    Modified bob.py to use words related to 100 Days of code.

---
+++ programs/99doc.py
@@ -0,0 +1,18 @@
+# 99 Days of Code - Sung to the tune of "99 bottles of beer"
+
+x = 99
+z = 1
+while x > 1:
+    y = x - 1
+    print(x , "days of code to complete,", x, "days of code.")
+    print("Commit to win,then start again", y, "days of code to complete...")
+    print()
+    x = x - 1
+
+if x == 1:
+    print("1 day of code to complete,", "1 day of code")
+    print("It wont be long so finish off strong, 1 day of code to complete.")
+
+print()
+print("No more days of code to complete, no more days of code")
+print("Don't be blue, just pick something new! 99 days of code to complete")
